dd8dc9f5be7d3c71e755304461fe3cc4a63d68a8
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-11-04 15:34:18 +0200

    Add files via upload

---
+++ 12_Spring_Data_Bookshop/src/main/java/com/example/demo/services/CategoryService.java
@@ -0,0 +1,8 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.Category;
+import java.util.Set;
+
+public interface CategoryService {
+    Set<Category> getRandomCategories();
+}
