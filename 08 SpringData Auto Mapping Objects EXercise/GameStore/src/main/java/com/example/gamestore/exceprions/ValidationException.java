11aa655f25591e006518437fd12a4186a3f43aed
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-11-13 15:47:28 +0200

    Delete 08 SpringData Auto Mapping Objects EXercise/GameStore directory

--- 08 SpringData Auto Mapping Objects EXercise/GameStore/src/main/java/com/example/gamestore/exceprions/ValidationException.java
+++
@@ -1,8 +0,0 @@
-package com.example.gamestore.exceprions;
-
-public class ValidationException extends RuntimeException{
-
-    public ValidationException(String reason){
-        super(reason);
-    }
-}
--- 08 SpringData Auto Mapping Objects EXercise/GameStore/src/main/java/com/example/gamestore/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gamestore.repositories;
-
-import com.example.gamestore.entities.users.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
